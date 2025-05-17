bdcdaf0ad27e6a0124c41c2bbbb111b51e862729
Салангин Дмитрий Андреевич <d.salangin@inlinegroup.ru>
2025-05-17 19:52:45 +0300

    chore: set dev dependencies

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import  { defineConfig, globalIgnores } from 'eslint/config';
+import  tsParser from '@typescript-eslint/parser';
+import  typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import  globals from 'globals';
+import  js from '@eslint/js';
+import  { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+
+    extends: compat.extends(
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ),
+
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  globalIgnores(['**/eslint.config.js']),
+]);
